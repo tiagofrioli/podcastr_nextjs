7cb368c4122c7f5b5295697bcab0b7874b856061
Tiago <tiago.frioli@gmail.com>
2021-04-24 01:23:09 -0300

    componentes/json-server/SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,51 @@
+import React, { useEffect } from "react";
 
-export default function Home() {
+
+export default function Home(props) {
+
+  /* ---------------------------------
+      SPA
+  
+  useEffect(()=>{
+
+    fetch('http://localhost:3333/episodes').then(response => response.json()
+    .then(data => console.log(data)));
+
+  }, []);
+ ----------------------------------------*/
+
+ 
   return (
-   <h1>Hello world</h1>
+      <h1>Hello</h1>
   )
 }
+
+/* SSG */
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json();
+
+  return{
+    props:{
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
+
+/* ----------------------------------------
+
+    SSR
+
+export async function getServerSideProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json();
+
+  return{
+    props:{
+      episodes: data
+    }
+  }
+}
+
+------------------------------------------*/
